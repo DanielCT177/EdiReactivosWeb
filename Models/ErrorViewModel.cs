4cf010ec1b7c73f36d6f062dc664f9097a4fc6bf
DanielCT177 <7731696150d@gmail.com>
2026-03-11 06:09:58 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EdiReactivosWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
